72dbdf81fc1ec164cf4fd22c7c7dd483ac6345d8
Perry Clark <highzinc@gmail.com>
2016-11-01 14:03:56 -0400

    find regions

--- ec2CSV.sh
+++ ec2CSV.sh
@@ -1,20 +1,23 @@
 #!/bin/bash
+export AWS_DEFAULT_REGION=us-east-1
 
-# EC2_REGIONS=("us-east-1" "us-west-1" "us-west-2" "eu-west-1" "ap-southeast-1" "ap-southeast-2" "ap-northeast-1" "sa-east-1")
-
-EC2_REGIONS=("us-east-1" "us-west-1" "us-west-2")
+EC2_REGIONS=$(aws ec2 describe-regions --query 'Regions[].{Name:RegionName}' --output text)
 
 for reg in ${EC2_REGIONS[@]}; do
 export AWS_DEFAULT_REGION=$reg
 
 INSTANCES=`aws ec2 describe-instances --query 'Reservations[].Instances[].InstanceId' --output text`
 
+if [[ -z $INSTANCES ]]; then
+echo "no instances in $reg"
+else
 for ID in $INSTANCES; do
+
+# We could probably use less aws calls...
 
 NAME=`aws ec2 describe-tags --filters "Name=resource-id,Values=$ID" "Name=key,Values=Name" | jq -r .Tags[].Value`
 SSHPORT=`aws ec2 describe-tags --filters "Name=resource-id,Values=$ID" "Name=key,Values=SSHPort" | jq -r .Tags[].Value`
 
-    # aws ec2 describe-instances --filters "Name=instance-id,Values=$ID" | jq -r "@csv \"$reg,${NAME},${SSHPORT},\(.Reservations[].Instances[] | [.InstanceId, .State.Name,.InstanceType,.PrivateIpAddress,.PublicIpAddress,.KeyName]) \""
 VALS=`aws ec2 describe-instances --filters "Name=instance-id,Values=$ID" | jq -r "@csv \"\(.Reservations[].Instances[] | [\"$reg\",\"${NAME}\",\"${SSHPORT}\",.InstanceId,.InstanceType,.PrivateIpAddress,.PublicIpAddress,.KeyName])\""`
 
 INSERTME="INSERT INTO xtnbu.instance(instance_regcode,instance_name, instance_sshport, instance_inid, instance_intype, instance_privip, instance_pubip, instance_keyname) VALUES ($VALS);"
@@ -22,9 +25,12 @@ INSERTME=$(echo $INSERTME | tr '"' "'")
 INSERTME=$(echo $INSERTME | sed -e 's/,,/,'NULL',/g')
 INSERTME=$(echo $INSERTME | sed -e 's/,,/,'NULL',/g')
 
-echo $INSERTME
-done
+cat << EOF >> ec2.sql
+$INSERTME
+EOF
 
+done
+fi
 
 
 done
